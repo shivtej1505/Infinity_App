b7e28a6b438a96dc85548c33a784d43698a9ffbf
shivtej1505 <15electronicmotor@gmail.com>
2015-09-12 17:40:14 +0530

    http done

--- app/src/main/java/infinity/beyond/MainActivity.java
+++ app/src/main/java/infinity/beyond/MainActivity.java
@@ -1,16 +1,57 @@
 package infinity.beyond;
 
 import android.app.Activity;
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+    import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textview);
+        new HttpsGet().execute();
+    }
+
+    private class HttpsGet extends AsyncTask<Void,Void,String>{
+
+        private static final String URL = "https://api.quikr.com/public/trending";
+        AndroidHttpClient client = AndroidHttpClient.newInstance("beyond");
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            HttpGet get = new HttpGet(URL);
+            get.setHeader("X-Quikr-App-Id","520");
+            get.setHeader("X-Quikr-Token-Id","2865896");
+            get.setHeader("X-Quikr-Signature","a8e02a923ef6aa3d3ae8d37975638ca7ed45fc63");
+            get.setHeader("Content-Type","application/json");
+
+            try {
+                ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                return client.execute(get,responseHandler);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "Error";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            textView.setText(s);
+        }
     }
 
     @Override
